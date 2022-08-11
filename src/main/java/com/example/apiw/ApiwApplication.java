282468d1e8e83f345e2fe49cab65824ca809a88c
Quan-92 <71068237+Quan-92@users.noreply.github.com>
2022-08-11 09:51:52 +0700

    first commit

---
+++ src/main/java/com/example/apiw/ApiwApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiwApplication.class, args);
+    }
+
+}
